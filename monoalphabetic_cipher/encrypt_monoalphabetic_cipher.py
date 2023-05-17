b83c1ecd19074019c8290775b9e29a538eea55ca
ak4m410x01 <abdallah10kamal@gmail.com>
2023-05-17 15:08:08 +0300

    encrypt_monoalphabetic_cipher

---
+++ monoalphabetic_cipher/encrypt_monoalphabetic_cipher.py
@@ -0,0 +1,25 @@
+#### Monoalphabetic_Cipher Encryption ####
+
+
+def encrypt_monoalphabetic_cipher(plain: str, letters: str, key: str) -> str:
+    plain = plain.lower()
+    cipher = ""
+
+    for i in plain:
+        idx = letters.find(i)
+
+        if idx == -1:
+            cipher += "?"
+        else:
+            cipher += key[idx]
+
+    return cipher
+
+
+#### Monoalphabetic_Cipher Decryption ####
+
+# letters = "abcdefghijklmnopqrstuvwxyz"
+# key = "qwertyuiopasdmghjklzxcvbnf"
+# plain = "Hello"
+# cipher = encrypt_monoalphabetic_cipher(plain, letters, key)
+# print(f"{plain}:{cipher}")
